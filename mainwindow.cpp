6bb3f481763a8a0fba422935d73a1556d7b9f367
Ilya Kidyaev <ilyakud667@gmail.com>
2024-11-20 22:12:15 +0200

    Updates files

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,19 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //Test buttons
+    QPushButton *button = new QPushButton(ui->scrollArea->widget());
+    button->setStyleSheet("background-color:red;");
+    button->setGeometry(0, 0, 390, 100);
+    button->update();
+
+    QPushButton *button2 = new QPushButton(ui->scrollArea->widget());
+    button2->setStyleSheet("background-color:green;");
+    button2->setGeometry(0, 100, 390, 100);
+    button2->update();
+
+    ui->scrollArea->widget()->setMinimumHeight(1000);
+    ui->scrollArea->setWidgetResizable(true);
 }
 
 MainWindow::~MainWindow()
